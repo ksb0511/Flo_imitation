701a1e22960f9a4f623cac8aac9412b7aac26ef0
ksb0511 <soobin796@gmail.com>
2022-04-06 14:57:52 +0900

    filing

--- app/src/main/java/com/ksb/myapplication/service/MusicService.kt
+++ app/src/main/java/com/ksb/myapplication/service/MusicService.kt
@@ -5,6 +5,12 @@ import retrofit2.http.GET
 
 interface MusicService {
 
+
     @GET("/v3/9f853a2a-62b3-48f4-91d5-65eabd0b32f5")
     fun listMusics() : Call<MusicDto>
+}
+
+abstract class func {
+    final var x = 0;
+    var y = 0;
 }
