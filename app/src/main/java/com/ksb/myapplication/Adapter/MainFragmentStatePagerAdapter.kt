71ff0fea0eda9d9439fbd06775481674cf4df9f8
ksb0511 <soobin796@gmail.com>
2022-04-16 12:48:02 +0900

    error fix

--- app/src/main/java/com/ksb/myapplication/Adapter/MainFragmentStatePagerAdapter.kt
+++ app/src/main/java/com/ksb/myapplication/Adapter/MainFragmentStatePagerAdapter.kt
@@ -3,10 +3,12 @@ package com.ksb.myapplication.Adapter
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentStatePagerAdapter
-import com.ksb.myapplication.FirstFragment
-import com.ksb.myapplication.LastFragment
-import com.ksb.myapplication.SecondFragment
-import com.ksb.myapplication.ThirdFragment
+
+import com.ksb.myapplication.FragmentPage.FirstFragment
+import com.ksb.myapplication.FragmentPage.LastFragment
+import com.ksb.myapplication.FragmentPage.SecondFragment
+import com.ksb.myapplication.FragmentPage.ThirdFragment
+
 
 class MainFragmentStatePagerAdapter(fm : FragmentManager, val fragmentCount : Int) : FragmentStatePagerAdapter(fm) {
     override fun getItem(position: Int): Fragment {
